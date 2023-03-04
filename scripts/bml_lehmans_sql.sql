94d4aa9770a93d08bdc3b4e603974c490e728836
brandylyn <brandylynlanders@gmail.com>
2023-03-04 09:14:55 -0600

    beginning of class push

--- scripts/bml_lehmans_sql.sql
+++ scripts/bml_lehmans_sql.sql
@@ -144,4 +144,35 @@ GROUP BY playerID
 ORDER BY Success_Rate DESC
 LIMIT 5;
 
+--FOR ENTERTAINMENT WHICH COLLEGES PRODUCED THE MAJORITY OF PLAYERS-----------
+SELECT 
+  schoolName, 
+  COUNT(DISTINCT playerID) AS Player_Count 
+FROM schools 
+INNER JOIN collegeplaying ON schools.schoolID = collegeplaying.schoolID 
+GROUP BY schoolName 
+ORDER BY Player_Count DESC 
+LIMIT 10;
+----------------FUNS OVER-----BACK TO WORK
+
 -- 7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+
+SELECT DISTINCT teamid,name, MAX(W) AS Max_Wins 
+FROM teams 
+WHERE yearID BETWEEN 1970 AND 2016 AND W < (SELECT MAX(W) FROM teams WHERE yearID BETWEEN 1970 AND 2016 AND WSWin='N')
+GROUP BY teamid, name
+ORDER BY max_wins DESC
+-----114 wins by NYA---------
+
+SELECT DISTINCT teamid,name, MIN(W) AS MIN_Wins 
+FROM teams 
+WHERE yearID BETWEEN 1970 AND 2016 AND W < (SELECT MIN(W) FROM teams WHERE yearID BETWEEN 1970 AND 2016 AND WSWin='Y')
+GROUP BY teamid, name
+ORDER BY min_wins ASC;
+
+SELECT DISTINCT teamid, name, MIN(W) AS MIN_Wins 
+FROM teams 
+WHERE yearID BETWEEN 1970 AND 2016 AND WSWin='Y'
+GROUP BY teamid, name
+ORDER BY min_wins ASC
+LIMIT 1;
